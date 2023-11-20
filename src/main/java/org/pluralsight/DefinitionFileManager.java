ada52ed9f33eaa3013e7b7d9ce9fe8c2bafca548
Kerry Metayer <KMetayer@appdev.yearup.org>
2023-11-20 17:22:31 -0500

    added an array list for dictionary

---
+++ src/main/java/org/pluralsight/DefinitionFileManager.java
@@ -0,0 +1,12 @@
+package org.pluralsight;
+
+import java.util.ArrayList;
+
+public class DefinitionFileManager {
+
+    public ArrayList<Definition> getDictionary() {
+        ArrayList<Definition> dictionary = new ArrayList<>();
+        //TODO READ DATA FILES!
+        return dictionary;
+    }
+}
